db3bee00e0f5df540a9220580af7453f7d0b249c
diogosoares22 <diogo.sousa.soares2000@gmail.com>
2020-12-17 11:31:50 +0000

    added model interface, with minor restrictions that all models must have

---
+++ model_interface.py
@@ -0,0 +1,14 @@
+from simulation import Simulation
+
+class ModelInterface:
+    def __init__(self, data: list) -> None:
+        """ Creates the class instance"""
+        pass
+    
+    def preprocess_data(self) -> None:
+        """Does preprocessing associated with trading model"""
+        pass
+    
+    def execute(self, simul: Simulation) -> None:
+        """Executes actions for a certain day in the respective Simulation"""
+        pass

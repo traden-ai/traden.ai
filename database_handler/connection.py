20d04fc6e862c013893e22c16ef1e02b4623ed7f
diogosoares22 <diogo.sousa.soares2000@gmail.com>
2021-03-04 00:11:49 +0000

    Developed database handler for mongodb

---
+++ database_handler/connection.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+def get_client():
+    password = "StockTradingSim"
+    db_name = "StockData"
+    uri = "mongodb+srv://Admin:{}@cluster0.em20u.mongodb.net/{}?retryWrites=true&w=majority".format(password, db_name)
+    return MongoClient(uri) 
+
+def get_database(database):
+    return get_client()[database]
+
+def get_collection(database, collection):
+    return get_database(database)[collection]

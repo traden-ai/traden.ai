d31f9a9e041fd93db2e4b485e967cb1a40e179a3
diogosoares22 <diogo.sousa.soares2000@gmail.com>
2021-03-04 17:38:20 +0000

    finished mongo db handler

---
+++ database_handler/handler_calls.py
@@ -0,0 +1,24 @@
+from utils import insert_items, remove_multiple, remove_item, query_time, get_entire_collection, update_item
+
+def insert_data(items):
+    insert_items(items)
+
+def get_all_data(tickers):
+    return get_entire_collection()
+
+def get_data(tickers):
+    data = []
+    for ticker in tickers:
+        data += [query_item(ticker)]
+    return data
+
+def update_data(items):
+    for item in items:
+        update_item(item["Symbol"], item["Data"])
+
+def delete_all_data():
+    remove_multiple({})
+
+def delete_data(tickers):
+    for ticker in tickers:
+        remove_item(ticker)
